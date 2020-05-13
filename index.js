d093dc5df4d747231855e61c33a6dd6878ba0955
Ahmed Jamal <engahmedd96@gmai.com>
2020-05-13 23:00:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function writeCards(gifts){
+  let a=gifts.length;
+  for(let i=0;i<a;i++){
+    gifts[i]=( `Thank you, ${gifts[i]}, for the wonderful surprise gift!`);
+  }
+  return gifts;
+}
+gifts=['Lisa','Kaitlin','Jan' ];
+ writeCards(gifts);
